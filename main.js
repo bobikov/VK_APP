af35704df7f78aa822e35acdf43d485499f4a28a
Bobikov <kostyabobikov@gmail.com>
2015-06-19 20:26:46 +0500

    ok

--- main.js
+++ main.js
@@ -62,18 +62,7 @@ VK.Auth.getLoginStatus(authInfo);
 			function(data){
 			if(data.response){
 				var obj = data.response;
-			
 
-					// $(".status3").html(
-					// '<table border="1" cellspacing="0" cellpadding="10">'+
-					// '<tr><td>album_id:</td><td>' + obj.aid+ "</td></tr>" + 
-					// '<tr><td>owner_id:</td><td> ' + obj.owner_id+ "</td></tr>" + 
-					// '<tr><td>title:</td><td> ' + obj.title+ "</td></tr>" +
-					// '<tr><td>thumb_id:</td><td> ' + obj.thumb_id+ "</td></tr>"+
-					// '<tr><td>size:</td><td> ' + obj.size+ "</td></tr>"+
-					// "</table>"
-					// );
-				// $("<div>"+key + element.owner_id+"</div>").appendTo($(".status3"));
 				$.each(obj, function(key, element){
 					$("<table style='float:left' border='1' cellpadding='6' cellspacing='0'><tr><td>Title</td><td>"+element.title+"<tr><td>AlbumID</td><td>" + element.aid + "</td></tr><tr><td>Size</td><td>" + element.size + "</td></tr></table>").appendTo($(".status3"));
 				});
